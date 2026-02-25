fbbdff9c2f246b5ab6cd47bce97060d5b4cff200
garrettdw <garrettdw123@icloud.com>
2026-02-25 14:03:35 -0500

    lec

--- 10-testing-your-functions/tests/test_trapezoids.py
+++ 10-testing-your-functions/tests/test_trapezoids.py
@@ -1,5 +1,4 @@
 from trapezoids import trapezint
-
 def hat(x):
     """Simple function that equals x between [0,1], 2-x between [1,2], and zero otherwise"""
     if 0 <= x < 1:
@@ -13,3 +12,15 @@ def test_trapezint_hat04():
     """Can we integrate a hat function from 0 to 4 with four trapezoids?"""
     result = trapezint(hat, 0, 4)
     assert result == 1.0
+
+def test_trapezint_hat01():
+    """Can we integrate a step function from 0 to 1 with one trapezoid?"""
+    result = trapezint(hat, 0, 1)
+    assert result == 0.5
+
+
+def test_trapezint_sin0pi():
+    import numpy as np
+    from numpy import sin, pi
+    result = trapezint(np.sin,0,np.pi,n=2)
+    assert abs(result - np.pi/2) < 1e-5 
